a14abe27878d6645178ac8558c8ba9ff2458810b
lecristina <cristinale33@hotmail.com>
2024-09-30 11:20:45 -0300

    Adicionado conteúdo na página Home e estilização de: home, cabecalho e rodape.

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar'
 
 export default function Cabecalho() {
   return (
-    <header>
+    <header className="cabecalho">
         <h1>Worlds in Collision</h1>
         <h2>Immanuel Velikovsky</h2>
         <Navbar/>
