59979e65c67a40110674a152d45958c8ce9d4480
p3terhenry <pedro.antonieti@outlook.com>
2024-09-30 12:26:35 -0300

    Criação e estilização da página de cálculos

--- my-app/src/app/calculos/page.tsx
+++ my-app/src/app/calculos/page.tsx
@@ -1,9 +1,40 @@
+import Link from 'next/link';
 import React from 'react'
 
 export default function Calculos() {
+  
+  const calculos = [
+    {id:1,nome: 'Atração Gravitacional', description: 'Velikovsky sugeriu que a interação gravitacional entre planetas poderia causar catástrofes na Terra.'},
+    {id:2,nome: 'Distância de Astros', description: ''}, 
+  ];
+  
   return (
-    <div>
-      <h1>Calculos</h1>
+    <div className="p-6 bg-white rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold mb-4">Cálculos</h1>
+      <table className="min-w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th className="item-table">ID</th>
+            <th className="item-table">Nome do Cálculo</th>
+            <th className="item-table">Descrição</th>
+            <th className="item-table">Acessar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {calculos.map((cal) => (
+            <tr key={cal.id} className="hover:bg-gray-50">
+              <td className="item-table">{cal.id}</td>
+              <td className="item-table">{cal.nome}</td>
+              <td className="item-table">{cal.description}</td>
+              <td className="item-table">
+                <Link href={`/calculos/${cal.id}`} className="text-blue-500 hover:underline">
+                  Acessar
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
