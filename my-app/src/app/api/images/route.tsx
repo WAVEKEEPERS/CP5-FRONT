6988ba9e63f22563e7f6e2549b299f0a30b06e1a
P3terHenry <pedro.antonieti@outlook.com>
2024-09-30 23:42:35 -0300

    Criação do endpoint get para consumo da API da Nasa.

--- my-app/src/app/api/images/route.tsx
+++ my-app/src/app/api/images/route.tsx
@@ -1,31 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from "next/server";
 
-interface NasaImage {
-  title: string;
-  url: string;
-  explanation: string;
-  media_type: string;
+export async function GET(request: Request) {
+  const retorno = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=SNGJ5ksL523nY4Fcrp1ohQss7khXfLcQhdD7WgFa&count=5`
+  );
+
+  const images = await retorno.json();
+
+  return NextResponse.json(images);
 }
-
-export default async (req: NextApiRequest, res: NextApiResponse<NasaImage[] | { message: string }>) => {
-  // Verifica se o método é GET
-  if (req.method !== 'GET') {
-    res.setHeader('Allow', ['GET']);
-    return res.status(405).end(`Método ${req.method} não permitido`);
-  }
-
-  try {
-    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=2T033JR8Pz0ggh6Qz0mq116aawZ3bPEavsK4NIgq&count=5');
-    if (!response.ok) {
-      throw new Error('Erro ao buscar imagens da NASA');
-    }
-    const data: NasaImage[] = await response.json();
-
-    // Retorna a lista de imagens
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error('Erro ao buscar imagens da NASA:', error);
-    return res.status(500).json({ message: 'Erro ao buscar imagens' });
-  }
-};
-
