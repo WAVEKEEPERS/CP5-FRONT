f2be162a929ee5b8d72e052a6a8fa8c97a2bfb3a
P3terHenry <pedro.antonieti@outlook.com>
2024-09-30 23:42:59 -0300

    Criação da página da Nasa para exibição do consumo da API.

--- my-app/src/app/nasa/page.tsx
+++ my-app/src/app/nasa/page.tsx
@@ -1,58 +1,55 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 interface NasaImage {
+  copyright: string;
+  date: string;
+  explanation: string;
   title: string;
   url: string;
-  explanation: string;
-  media_type: string;
+  hdurl: string;
 }
 
-const NasaImages = () => {
-  const [images, setImages] = useState<NasaImage[]>([]);
-  const [error, setError] = useState<string | null>(null);
+export default function Nasa() {
+  const [lista, setLista] = useState<NasaImage[]>([]);
 
   useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch('/api/images');
-        if (!response.ok) {
-          throw new Error('Erro ao buscar imagens');
-        }
-        const data: NasaImage[] = await response.json();
-        setImages(data);
-      } catch (error) {
-        // Verificando se o erro é do tipo Error
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError('Erro desconhecido');
-        }
-      }
+    const chamadaApi = async () => {
+      const response = await fetch("http://localhost:3000/api/images");
+      const dados = await response.json();
+      setLista(dados);
     };
-
-    fetchImages();
+    chamadaApi();
   }, []);
 
   return (
-    <div>
-      <h1>Imagens da NASA</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <div>
-        {images.map((image) => (
-          <div key={image.title}>
-            <h2>{image.title}</h2>
-            {image.media_type === 'image' ? (
-              <img src={image.url} alt={image.title} style={{ width: '300px' }} />
-            ) : (
-              <iframe src={image.url} title={image.title} width="300" height="200" />
-            )}
-            <p>{image.explanation}</p>
-          </div>
-        ))}
-      </div>
+    <div className="p-6 bg-white">
+      <h1 className="text-3xl font-bold mb-4">Nasa Imagens</h1>
+      <table className="min-w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th className="item-table">Título</th>
+            <th className="item-table">Exploração</th>
+            <th className="item-table">URL da Imagem (Baixa Resolução)</th>
+            <th className="item-table">URL da Imagem (Alta Resolução)</th>
+            <th className="item-table">Data</th>
+            <th className="item-table">Direitos Autorais</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lista.map((info) => (
+            <tr key={info.date} className="text-center hover:bg-gray-50">
+              <td className="item-table">{info.title}</td>
+              <td className="item-table">{info.explanation}</td>
+              <td className="text-green-500 hover:underline"><Link href={info.url}>Clique Aqui</Link></td>
+              <td className="text-blue-500 hover:underline"><Link href={info.hdurl}>Clique Aqui</Link></td>
+              <td className="item-table">{info.date}</td>
+              <td className="item-table">{info.copyright}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
-};
-
-export default NasaImages;
+  )
+}
